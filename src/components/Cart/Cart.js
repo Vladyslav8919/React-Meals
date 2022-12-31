8c86643bfb336b456a1ac9fdd7ef27189770b267
Vladyslav8919 <vladyslav.pshevlotskyi@gmail.com>
2022-12-31 17:47:42 +0200

    v2

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,124 @@
+import React, { useState, useContext, Fragment } from "react";
+
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
+
+const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+  const [error, setError] = useState(null);
+
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(
+        "https://react-meals-20bdd-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+        {
+          method: "POST",
+          body: JSON.stringify({ user: userData, orderedItems: cartCtx.items }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      setIsSubmitting(false);
+      setDidSubmit(true);
+      cartCtx.clearCart();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <Fragment>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data</p>;
+
+  const failedSubmitModalContent = (
+    <p>{`An error (${error}) occured while sending your order. Please try again!`}</p>
+  );
+
+  const didSumbitModalContent = (
+    <Fragment>
+      <p>Successfully sent the order</p>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </Fragment>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && !failedSubmitModalContent && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSumbitModalContent}
+      {error && failedSubmitModalContent}
+    </Modal>
+  );
+};
+
+export default Cart;
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+// const DUMMY_MEALS = [
+//   {
+//     id: 'm1',
+//     name: 'Sushi',
+//     description: 'Finest fish and veggies',
+//     price: 22.99,
+//   },
+//   {
+//     id: 'm2',
+//     name: 'Schnitzel',
+//     description: 'A german specialty!',
+//     price: 16.5,
+//   },
+//   {
+//     id: 'm3',
+//     name: 'Barbecue Burger',
+//     description: 'American, raw, meaty',
+//     price: 12.99,
+//   },
+//   {
+//     id: 'm4',
+//     name: 'Green Bowl',
+//     description: 'Healthy...and green...',
+//     price: 18.99,
+//   },
+// ];
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      // setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          "https://react-meals-20bdd-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+        );
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const data = await response.json();
+
+        const loadedMeals = [];
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        setMeals(loadedMeals);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  let content = <p>Found no meals.</p>;
+
+  if (meals.length > 0) {
+    content = <ul>{mealsList}</ul>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <section className={classes.meals}>
+      <Card>{content}</Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
